27c102ffc2f30ee92cb5355197ef6093d76e1afa
YosefAbsal <yosef.absal@gmail.com>
2025-11-19 18:22:35 -0700

    First commit

---
+++ Server/server.js
@@ -0,0 +1,43 @@
+const http = require('node:http');
+const rUUID = require('node:crypto');
+const hostname = '127.0.0.1';
+const port = '5200';
+
+let urlMap = new Map();
+const server = http.createServer((req, res) => {
+
+    if (!req.url.includes("/go/")) {
+        let newId = rUUID.randomInt(1000000);
+        while (urlMap.has(newId)) {
+            newId = rUUID.randomInt(1000000);
+        }
+
+        let sentUrl = req.url.replace("/", "");
+        urlMap.set(newId.toString(), sentUrl);
+
+        res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:5500');
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify(`http://127.0.0.1:5200/go/${newId}`));
+
+    }
+    else {
+        let id = req.url.replace("/go/", "");
+        let goToUrl = urlMap.get(id);
+
+        try {
+            res.writeHead(301, {
+                location: goToUrl
+            })
+        }
+        catch(err) {
+            console.error(err);
+        }
+        
+        res.end();
+    }
+
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+})
