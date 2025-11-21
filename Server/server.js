ccf6435df363a2dea9e3dd2eb4e4098083deb3d3
YosefAbsal <yosef.absal@gmail.com>
2025-11-21 16:50:00 -0700

    index.html and server.js changes

--- Server/server.js
+++ Server/server.js
@@ -1,43 +1,40 @@
 const http = require('node:http');
-const rUUID = require('node:crypto');
-const hostname = '127.0.0.1';
-const port = '5200';
+const crypto = require('node:crypto');
+
+const port = process.env.PORT || 5200;
 
 let urlMap = new Map();
 const server = http.createServer((req, res) => {
 
     if (!req.url.includes("/go/")) {
-        let newId = rUUID.randomInt(1000000);
+        let newId = crypto.randomInt(1000000);
         while (urlMap.has(newId)) {
-            newId = rUUID.randomInt(1000000);
+            newId = crypto.randomInt(1000000);
         }
 
         let sentUrl = req.url.replace("/", "");
         urlMap.set(newId.toString(), sentUrl);
 
-        res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:5500');
+        res.setHeader('Access-Control-Allow-Origin', 'https://yosefabsal.github.io');
         res.setHeader('Content-Type', 'application/json');
-        res.end(JSON.stringify(`https://yosefabsal.github.io/go/${newId}`));
+        res.end(JSON.stringify(`https://masurlshort.onrender/go/${newId}`));
 
     }
     else {
         let id = req.url.replace("/go/", "");
         let goToUrl = urlMap.get(id);
 
-        try {
-            res.writeHead(301, {
-                location: goToUrl
-            })
-        }
-        catch(err) {
-            console.error(err);
+        if (!goToUrl) {
+            res.writeHead(404, { "Content-Type": "text/plain" });
+            return res.end("Invalid short URL");
         }
         
+        res.writeHead(301, { location: goToUrl });
         res.end();
     }
 
 });
 
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
+server.listen(port, () => {
+    console.log(`Server running at ${port}/`);
 })
